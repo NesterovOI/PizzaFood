c883bab6f7efe0637b4d7b9e826a2ea883019f29
Admin <admin@Admins-MacBook-Pro.local>
2023-01-30 19:54:56 +0200

    Title String in data class Food

--- app/src/main/java/com/nesterov/pizza/data/Food.kt
+++ app/src/main/java/com/nesterov/pizza/data/Food.kt
@@ -1,12 +1,11 @@
 package com.nesterov.pizza.data
 
-import android.graphics.drawable.Drawable
 import android.os.Parcel
 import android.os.Parcelable
 
 data class Food(
     var image: Int,
-    var title: Int,
+    var title: String?,
     var description: Int,
     var money: Double,
     var numberInCart: Int,
@@ -14,7 +13,7 @@ data class Food(
 ):Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
-        parcel.readInt(),
+        parcel.readString(),
         parcel.readInt(),
         parcel.readDouble(),
         parcel.readInt(),
@@ -24,14 +23,14 @@ data class Food(
 
     constructor(
         image: Int,
-        title: Int,
+        title: String,
         description: Int,
         money: Double
     ) : this(image, title, description, money, numberInCart = 0, totalMoney = 0.0)
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(image)
-        parcel.writeInt(title)
+        parcel.writeString(title)
         parcel.writeInt(description)
         parcel.writeDouble(money)
         parcel.writeInt(numberInCart)
