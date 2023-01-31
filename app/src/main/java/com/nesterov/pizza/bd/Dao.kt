445c0f4cc9e14ab46198d0e874955bf2fe744859
Admin <admin@Admins-MacBook-Pro.local>
2023-01-31 18:53:16 +0200

    add build.gradle implementation lifecycle and add in interface Dao Flow

--- app/src/main/java/com/nesterov/pizza/bd/Dao.kt
+++ app/src/main/java/com/nesterov/pizza/bd/Dao.kt
@@ -4,11 +4,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.nesterov.pizza.data.FoodCart
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface Dao {
     @Insert
     fun insertFoodCart(foodCart: FoodCart)
     @Query("SELECT * FROM foodCart")
-    fun getAllFoodCart(): List<FoodCart>
+    fun getAllFoodCart(): Flow<List<FoodCart>>
 }
